53107d9e4f9b1270610bb0269b393276f2d260ff
branchwag <branchleannelmb@gmail.com>
2025-04-15 20:53:54 -0400

    removes extra Swing loading in main method to simplify app

--- com/celestialdistance/CelestialDistanceCalculator.java
+++ com/celestialdistance/CelestialDistanceCalculator.java
@@ -2,25 +2,12 @@ package com.celestialdistance;
 
 import com.celestialdistance.view.MainFrame;
 
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-
 //main class to launch app
 public class CelestialDistanceCalculator {
-	//main method
+	// main method
 	public static void main(String[] args) {
-		try {
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {
-			System.err.println("Error: " + e);
-		}
 
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				MainFrame mainFrame = new MainFrame();
-				mainFrame.setVisible(true);
-			}
-		});
+		MainFrame mainFrame = new MainFrame();
+		mainFrame.setVisible(true);
 	}
 }
